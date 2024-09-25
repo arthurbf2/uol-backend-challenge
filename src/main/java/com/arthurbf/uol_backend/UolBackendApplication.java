3d020aded4955375a315575e6c90e08137ab99d6
arthurbf2 <arthurfalcao2010@gmail.com>
2024-09-25 15:53:26 -0300

    create repo

---
+++ src/main/java/com/arthurbf/uol_backend/UolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arthurbf.uol_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolBackendApplication.class, args);
+	}
+
+}
